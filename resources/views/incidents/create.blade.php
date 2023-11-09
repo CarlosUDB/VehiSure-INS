2bcedb8c7890e3723220a3286dd87b2c19128441
Manuel Marcía <manuel.marcia@shopstack.io>
2023-11-08 23:21:01 -0600

    feat: adds stetical changes on incidents form

--- resources/views/incidents/create.blade.php
+++ resources/views/incidents/create.blade.php
@@ -1,102 +1,138 @@
 <x-app-layout>
-    <div class="container">
+    <style>
+        .breadcrumb {
+            background-color: transparent;
+            padding: 0;
+            margin: 0;
+        }
+
+        .breadcrumb-item {
+            padding: 0;
+            margin: 0;
+            font-size: 0.9rem;
+        }
+
+        .breadcrumb-item.active {
+            color: #007bff;
+        }
+
+        .breadcrumb-item.active::before {
+            content: "";
+        }
+
+        .row {
+            display: flex;
+            justify-content: space-between;
+            align-items: center;
+        }
+    </style>
+    <div class="container mt-3">
+        <div class="row mb-3">
+            <div class="col-md-6">
+                <nav aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="/incidents">Página anterior /</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Página actual</li>
+                    </ol>
+                </nav>
+            </div>
+        </div>
         <form action="{{ route('incidents.store') }}" method="POST">
             @csrf
             <div class="card">
-                <div class="card-header">
-                    <h2>Crear incidente</h2>
+                <div class="card-header bg-primary text-white">
+                    <h2 class="mb-0">Crear Incidente</h2>
                 </div>
-                <div class="card-body">         
-                    <div class="form-row justify-content-center align-items-center">
-                        <div class="">
-
-                            <div class="form-outline mb-4">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
                                 <label for="date">Fecha</label>
-                                <span class="form-control-feedback"></span>
-                                <input name="date" class="form-control" id="datepicker"/>                                            
+                                <input name="date" class="form-control" id="datepicker" />
                                 @error('date')
-                                    <span class=”invalid-feedback” role=”alert”>
-                                        <i class="fa fa-exclamation" aria-hidden="true" class="text-danger"></i>
+                                    <div class="invalid-feedback">
+                                        <i class="fa fa-exclamation text-danger" aria-hidden="true"></i>
                                         <strong class="text-danger">{{ $message }}</strong>
-                                    </span>
+                                    </div>
                                 @enderror
                             </div>
 
-                            <div class="form-outline mb-4">
+                            <div class="form-group">
                                 <label for="description">Descripción</label>
-                                <span class=" form-control-feedback"></span>
-                                <textarea class="form-control" name="description" cols="4" id="description"></textarea>                                                     
+                                <textarea class="form-control" name="description" cols="4" id="description"></textarea>
                                 @error('description')
-                                    <span class=”invalid-feedback” role=”alert”>
-                                        <i class="fa fa-exclamation" aria-hidden="true" class="text-danger"></i>
+                                    <div class="invalid-feedback">
+                                        <i class="fa fa-exclamation text-danger" aria-hidden="true"></i>
                                         <strong class="text-danger">{{ $message }}</strong>
-                                    </span>
+                                    </div>
                                 @enderror
                             </div>
-
-                            <div class="form-outline mb-4">
-                                <label for="police_report_number">Número de reporte policial</label>
-                                <span class="form-control-feedback"></span>
-                                <input type="number" name="police_report_number" min="0" step="1" value="0" class="form-control">
-                                 
-                                @error('police_report_number')
-                                    <span class=”invalid-feedback” role=”alert”>
-                                        <i class="fa fa-exclamation" aria-hidden="true" class="text-danger"></i>
-                                        <strong class="text-danger">{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>  
-
-                            <div class="form-outline mb-4">
-                                <label for="address">Ubicación</label>
-                                <span class="form-control-feedback"></span>
-                                <input type="text" class="form-control" name="address" id="address" placeholder="" value="{{ old('address') }}">                                                       
-                                @error('address')
-                                    <span class=”invalid-feedback” role=”alert”>
-                                        <i class="fa fa-exclamation" aria-hidden="true" class="text-danger"></i>
-                                        <strong class="text-danger">{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-
-                            <div class="form-outline mb-4">
-                                <label for="car_plate_number">Número de placa</label>
-                                <span class="form-control-feedback"></span>
-                                <input type="text" class="form-control" name="car_plate_number" id="car_plate_number" placeholder="" value="{{ old('car_plate_number') }}">                                                       
-                                @error('car_plate_number')
-                                    <span class=”invalid-feedback” role=”alert”>
-                                        <i class="fa fa-exclamation" aria-hidden="true" class="text-danger"></i>
-                                        <strong class="text-danger">{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-        
-                            <div class="form-outline mb-4">
-                                <div class="form-group">
-                                    <label for="category">Taller</label>
-                                    <select class="form-control" id="workshop_id" name="workshop_id">
-                                        @foreach ($workshops as $workshop)
-                                            <option value="{{ $workshop->id }}">{{ $workshop->name }}</option>
-                                        @endforeach
-                                   </select>
-                                </div>
-                                 
-                                @error('workshop_id')
-                                    <span class=”invalid-feedback” role=”alert”>
-                                        <i class="fa fa-exclamation" aria-hidden="true" class="text-danger"></i>
-                                        <strong class="text-danger">{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>  
-
-                            
                         </div>
-                    </div>    
-                    <div class="form-row justify-content-center align-items-center">
-                        <button type="submit" class="btn btn-primary">Crear</button>
-                    </div>                 
+
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="address">Ubicación</label>
+                                <input type="text" class="form-control" name="address" id="address" placeholder=""
+                                    value="{{ old('address') }}">
+                                @error('address')
+                                    <div class="invalid-feedback">
+                                        <i class="fa fa-exclamation text-danger" aria-hidden="true"></i>
+                                        <strong class="text-danger">{{ $message }}</strong>
+                                    </div>
+                                @enderror
+                            </div>
+
+                            <div class="form-group">
+                                <label for="car_plate_number">Número de placa</label>
+                                <input type="text" class="form-control" name="car_plate_number" id="car_plate_number"
+                                    placeholder="" value="{{ old('car_plate_number') }}">
+                                @error('car_plate_number')
+                                    <div class="invalid-feedback">
+                                        <i class="fa fa-exclamation text-danger" aria-hidden="true"></i>
+                                        <strong class="text-danger">{{ $message }}</strong>
+                                    </div>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="police_report_number">Número de reporte policial</label>
+                                <input type="number" name="police_report_number" min="0" step="1"
+                                    value="0" class="form-control">
+                                @error('police_report_number')
+                                    <div class="invalid-feedback">
+                                        <i class="fa fa-exclamation text-danger" aria-hidden="true"></i>
+                                        <strong class="text-danger">{{ $message }}</strong>
+                                    </div>
+                                @enderror
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
+                                <label for="category">Taller</label>
+                                <select class="form-control" id="workshop_id" name="workshop_id">
+                                    @foreach ($workshops as $workshop)
+                                        <option value="{{ $workshop->id }}">{{ $workshop->name }}</option>
+                                    @endforeach
+                                </select>
+                                @error('workshop_id')
+                                    <div class="invalid-feedback">
+                                        <i class="fa fa-exclamation text-danger" aria-hidden="true"></i>
+                                        <strong class="text-danger">{{ $message }}</strong>
+                                    </div>
+                                @enderror
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row mt-4">
+                        <div class="col-12 text-center">
+                            <button type="submit" class="btn btn-primary btn-lg">Crear</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </form>
     </div>
+
 </x-app-layout>
