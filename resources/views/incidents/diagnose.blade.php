3fa162705e418bfe29353e6bc668c8fc6c34ebee
Manuel Marcía <manuel.marcia@shopstack.io>
2023-11-09 00:07:02 -0600

    feat: adds stetical changes to last forms

--- resources/views/incidents/diagnose.blade.php
+++ resources/views/incidents/diagnose.blade.php
@@ -1,65 +1,63 @@
 <x-app-layout>
-    <div class="container">
+    <div class="container mt-3">
         <form action="{{ route('incidents.completeDiagnose', $incident->id) }}" method="POST">
             @csrf
             <div class="card">
-                <div class="card-header">
-                    <h2>Diagnosticando incidente</h2>
+                <div class="card-header bg-primary text-white">
+                    <h2 class="mb-0">Diagnosticando incidente</h2>
                 </div>
-                <div class="card-body">         
-                    <div class="form-row justify-content-center align-items-center">
-                        <div class="">
-
-                            <div class="form-outline mb-4">
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <div class="form-group">
                                 <label for="date">Fecha</label>
-                                <span class="form-control-feedback"></span>
-                                <input value='{{$incident->date}}' name="date" class="form-control" readonly/>                                            
-
+                                <input value='{{ $incident->date }}' name="date" class="form-control" readonly />
                             </div>
 
-                            <div class="form-outline mb-4">
+                            <div class="form-group">
                                 <label for="description">Descripción</label>
-                                <span class=" form-control-feedback"></span>
-                                <textarea readonly class="form-control" name="description" cols="4" id="description">{{$incident->description}}</textarea>                                                     
+                                <textarea readonly class="form-control" name="description" cols="4" id="description">{{ $incident->description }}</textarea>
                             </div>
 
-                            <div class="form-outline mb-4">
+                            <div class="form-group">
                                 <label for="police_report_number">Número de reporte policial</label>
-                                <span class="form-control-feedback"></span>
-                                <input readonly type="number" value='{{$incident->police_report_number}}' name="police_report_number" min="0" step="1" value="0" class="form-control">
-                            </div>  
-
-                            <div class="form-outline mb-4">
+                                <input readonly type="number" value='{{ $incident->police_report_number }}'
+                                    name="police_report_number" min="0" step="1" class="form-control">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="form-group">
                                 <label for="address">Ubicación</label>
-                                <span class="form-control-feedback"></span>
-                                <input readonly type="text" value='{{$incident->address}}' class="form-control" name="address" id="address" placeholder="" value="{{ old('address') }}">
+                                <input readonly type="text" value='{{ $incident->address }}' class="form-control"
+                                    name="address" id="address" placeholder="">
                             </div>
 
-                            <div class="form-outline mb-4">
+                            <div class="form-group">
                                 <label for="car_plate_number">Número de placa</label>
-                                <span class="form-control-feedback"></span>
-                                <input readonly type="text" value='{{$incident->car_plate_number}}' class="form-control" name="car_plate_number" id="car_plate_number" placeholder="" value="{{ old('car_plate_number') }}">
+                                <input readonly type="text" value='{{ $incident->car_plate_number }}'
+                                    class="form-control" name="car_plate_number" id="car_plate_number" placeholder="">
                             </div>
 
-                            <div class="form-outline mb-4">
+                            <div class="form-group">
                                 <label for="diagnosis">Diagnóstico</label>
-                                <span class=" form-control-feedback"></span>
                                 <textarea class="form-control" name="diagnosis" cols="4" id="diagnosis"></textarea>
                                 @error('diagnosis')
-                                    <span class=”invalid-feedback” role=”alert”>
-                                        <i class="fa fa-exclamation" aria-hidden="true" class="text-danger"></i>
+                                    <div class="invalid-feedback">
+                                        <i class="fa fa-exclamation text-danger" aria-hidden="true"></i>
                                         <strong class="text-danger">{{ $message }}</strong>
-                                    </span>
+                                    </div>
                                 @enderror
                             </div>
-                            
                         </div>
-                    </div>    
-                    <div class="form-row justify-content-center align-items-center">
-                        <button type="submit" class="btn btn-primary">Completar diagnóstico</button>
-                    </div>                 
+                    </div>
+                    <div class="row mt-4">
+                        <div class="col-12 text-center">
+                            <button type="submit" class="btn btn-primary">Completar diagnóstico</button>
+                        </div>
+                    </div>
                 </div>
             </div>
         </form>
     </div>
+
 </x-app-layout>
